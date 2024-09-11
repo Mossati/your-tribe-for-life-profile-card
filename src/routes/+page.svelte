4600cc970bae44d66992c70005a137543146c778
Mossati <xmossati9786@gmail.com>
2024-09-11 14:03:33 +0200

    directus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,48 @@
 <script>
-	/** @type {import('./$types').PageData} */
-	export let data;
-</script>
 
-<h1>{data.global.title}</h1>
-<p>{data.global.description}</p>
+  export let data
+ 
+  function checkAvatarImage (avatar) {
+
+    if (avatar.includes('https')) {
+
+      return true
+
+    }
+ 
+    return false
+
+  }
+</script>
+ 
+<main>
+ 
+  <ul>
+
+    {#each data.persons as person}
+<li>
+<span class="avatar">
+
+            {#if person.avatar.length > 0 && checkAvatarImage (person.avatar)}
+<img src="{person.avatar}" alt="Avatar van {person.name}">
+
+            {:else if person.avatar.length > 0 || checkAvatarImage (person.avatar)}
+
+              {person.avatar}
+
+            {:else}
+<img src="https://robohash.org/mail@ashallendesign.co.uk" alt="Avatar van {person.name}">
+
+            {/if}
+</span>
+ 
+          {person.name}
+</li>
+
+    {/each}
+</ul>
+</main>
+ 
+<style>
+</style>
+ 
